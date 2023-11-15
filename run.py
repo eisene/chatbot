ec211b834ec83923ee81721cba7f744de5ed5240
eisene <eeisenst@gmail.com>
2023-11-15 13:32:00 -0500

    Add memory to agent, extend example

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ from langchain.agents.format_scratchpad import format_to_openai_function_message
 from langchain.agents.output_parsers import OpenAIFunctionsAgentOutputParser
 from langchain.agents import AgentExecutor
 from flights_query_tool import get_flights, get_todays_date
+from langchain.schema.messages import AIMessage, HumanMessage
 
 
 AGENT_DEFINITION_PROMPT = \
@@ -15,18 +16,18 @@ AGENT_DEFINITION_PROMPT = \
 "flights.\n\n"
 "Don't ask the user if they would like to book the flights, just list them.\n\n"
 "You are a very powerful assistant but you don't know today's date and need to look it up with a tool."
+MEMORY_KEY = "chat_history"
 
 prompt = ChatPromptTemplate.from_messages(
     [
-        (
-            "system",
-            AGENT_DEFINITION_PROMPT,
-        ),
+        ("system", AGENT_DEFINITION_PROMPT),
+        MessagesPlaceholder(variable_name=MEMORY_KEY),
         ("user", "{input} Don't forget to look up today's date first if you need to!"),
         MessagesPlaceholder(variable_name="agent_scratchpad"),
     ]
 )
 
+chat_history = []
 tools = [get_flights, get_todays_date]
 llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
 # llm = ChatOllama(model="llama2", temperature=0)
@@ -38,6 +39,7 @@ agent = (
         "agent_scratchpad": lambda x: format_to_openai_function_messages(
             x["intermediate_steps"]
         ),
+        "chat_history": lambda x: x["chat_history"],
     }
     | prompt
     | llm_with_tools
@@ -46,4 +48,20 @@ agent = (
 
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-agent_executor.invoke({"input": "I need a flight from New York to Chicago tomorrow."})
+input_text = "I need a flight from New York to Chicago tomorrow."
+result = agent_executor.invoke({"input": input_text, "chat_history": chat_history})
+chat_history.extend(
+    [
+        HumanMessage(content=input_text),
+        AIMessage(content=result["output"]),
+    ]
+)
+
+input_text = "Actually, I would like to change the date to Nov 20th."
+result = agent_executor.invoke({"input": input_text, "chat_history": chat_history})
+chat_history.extend(
+    [
+        HumanMessage(content=input_text),
+        AIMessage(content=result["output"]),
+    ]
+)
